a2c857aeed9902ad031c6ec4a1c933e117c6b186
aaronrs2002 <aaronrs2002@yahoo.com>
2024-03-13 07:20:23 -0700

    Update server.js
    test syntax

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 
 const cors = require('cors');
 const corsOptions = {
-    origin: 'https://mechanized-aesthetics.net',
+    origin: 'https://mechanized-aesthetics.net/*',
     credentials: true,
     optionSuccessStatus: 200
 }
